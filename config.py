04478aa7b1502ecfb043136592b2ffaf0f4d3eee
VenkateshEnakonda7 <venkateshenakonda7.ev@gmail.com>
2025-04-29 20:41:25 +0530

    commented Auth

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-OPENROUTER_API_KEY = "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"
-GOOGLE_SHEETS_CREDENTIALS_FILE = "E:\Pyhton\AI_Job_Agent\\aijobagent-458205-8c7c44694ea4.json"
+# OPENROUTER_API_KEY = "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"
+# GOOGLE_SHEETS_CREDENTIALS_FILE = "E:\Pyhton\AI_Job_Agent\\aijobagent-458205-8c7c44694ea4.json"
 SHEET_NAME = "Job Applications"
 OLD_RESUME_PATH = "E:\Pyhton\AI_Job_Agent\Venkatesh_Enakonda_QA.docx"
 NEW_RESUME_DOCX_PATH = "E:\Pyhton\AI_Job_Agent\\new_resume.docx"
